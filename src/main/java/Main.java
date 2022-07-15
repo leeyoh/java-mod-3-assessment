a1cc2d2b69869e7a792e5a0f28dd0610ec12d206
John Lee <ljonghan@gmail.com>
2022-07-15 11:20:14 -0400

    added Jackson package.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        Person test = new Person("John","Lee",1999,10,10);
+        FileManagerSingleton fm = FileManagerSingleton.getInstance();
+        List<Person> plist = new ArrayList<Person>();
+        plist.add(test);
+        fm.saveAsJSON("test.json",plist);
     }
 }
